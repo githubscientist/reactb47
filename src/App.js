8adc08e24f40c49e23e54fbf644a75bb76107a8f
githubscientist <sathish.datascientist@gmail.com>
2023-06-09 21:30:52 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   // javascript
+//   // console.log('hello from component');
+
+//   const now = new Date();
+//   const a = 10;
+//   const b = 20;
+
+//   console.log(now, a + b);
+
+//   return (
+//     <div>
+//       <p>Hello World, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+
+function Hello(props) {
+  // console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you are { props.age } years old!</p>
+    </div>
+  )
+}
 
 function App() {
+
+  const age = 10;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Sathish' age={ 10 + 20 } />
+      <Hello name='Krish' age={ age } />
     </div>
-  );
+  )
 }
 
 export default App;
