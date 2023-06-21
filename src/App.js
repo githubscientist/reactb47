b6f2120c0b087123cbb16b9dca0fa72691d470e7
githubscientist <sathish.datascientist@gmail.com>
2023-06-21 11:25:09 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-// props drilling
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-function GrandChildComponent(props) {
+// create a context
+const MessageContext = createContext();
+const ChildMessageContext = createContext();
+
+function GrandChildComponent() {
+  const message = useContext(ChildMessageContext);
   return (
     <div>
       <h3>GrandChildComponent</h3>
-      <p>{ props.message }</p>
+      <p>{ message }</p>
     </div>
   )
 }
 
-function ChildComponent(props) {
-  const message = 'Hello, GrandChild!';
+function ChildComponent() {
+
+  // use the context data
+  const message = useContext(MessageContext);
+  const childMessage = 'hello from child';
+
   return (
     <div>
       <h2>Child Component</h2>
-      <p>{props.message}</p>
-      <GrandChildComponent message={message} />
+      <p>{message}</p>
+      <ChildMessageContext.Provider value={childMessage}>
+        <GrandChildComponent />
+      </ChildMessageContext.Provider>
     </div>
   )
 }
 
 function App() {
-
-  // parent component data
-  const message = 'Hello, Child!';
-
+  const [message, setMessage] = useState('Hello from App');
   return (
     <div>
       <h1>Parent Component</h1>
-      <ChildComponent message={ message } />
+      <MessageContext.Provider value={message}>
+        <ChildComponent />
+      </MessageContext.Provider>
     </div>
   )
 }
