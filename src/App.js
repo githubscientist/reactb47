a183e6651b4738064836a03b387e9224df2701ab
githubscientist <sathish.datascientist@gmail.com>
2023-06-21 10:46:44 +0530

    props drilling

--- src/App.js
+++ src/App.js
@@ -1,85 +1,36 @@
-// without React Router
-import React, { useState } from 'react';
-import { Link, Route, BrowserRouter as Router, Routes, useParams } from 'react-router-dom';
+// props drilling
+import React from 'react';
 
-function Home() {
+function GrandChildComponent(props) {
   return (
     <div>
-      <h2>Notes App</h2>
+      <h3>GrandChildComponent</h3>
+      <p>{ props.message }</p>
     </div>
   )
 }
 
-function Note({notes}) {
-  const id = useParams().id;
-  const note = notes.find(n => n.id === Number(id));
-
+function ChildComponent(props) {
+  const message = 'Hello, GrandChild!';
   return (
     <div>
-      <h2>{note.content}</h2>
-    </div>
-  )
-}
-
-function Notes({ notes }) {
-  return (
-    <div>
-      <h2>Notes</h2>
-      <ul>
-        {
-          notes.map(note => 
-            <li key={note.id}><Link to={`/notes/${note.id}`}>{ note.content }</Link></li>
-          )
-        }
-      </ul>
-    </div>
-  )
-}
-
-function Users() {
-  return (
-    <div>
-      <h2>Users App</h2>
+      <h2>Child Component</h2>
+      <p>{props.message}</p>
+      <GrandChildComponent message={message} />
     </div>
   )
 }
 
 function App() {
 
-  const notes = [
-    {
-      id: 1,
-      content: 'Javascript'
-    },
-    {
-      id: 2,
-      content: 'ReactJS'
-    },
-    {
-      id: 3,
-      content: 'NodeJS'
-    }
-  ];
-
-  const padding = {
-    padding: 10
-  };
+  // parent component data
+  const message = 'Hello, Child!';
 
   return (
-    <Router>
-      <div>
-        <Link to='/' style={padding}>home</Link>
-        <Link to='/notes' style={padding}>notes</Link>
-        <Link to='/users' style={padding}>users</Link>
-      </div>
-
-      <Routes>
-        <Route path='/notes/:id' element={ <Note notes={notes}/> } />
-        <Route path='/users' element={ <Users /> } />
-        <Route path='/notes' element={<Notes notes={notes} /> } />
-        <Route path='/' element={ <Home /> } />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Parent Component</h1>
+      <ChildComponent message={ message } />
+    </div>
   )
 }
 
