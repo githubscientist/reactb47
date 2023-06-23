70700fa00c0698d491d9ef8e5196eee997fdddb2
githubscientist <sathish.datascientist@gmail.com>
2023-06-23 22:01:11 +0530

    refactored+router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
-// A Simple Note taking Application
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
-
-function Note({note}) {
-  return (
-    <li>{ note.content }</li>
-  )
-}
+import CreateNote from './components/CreateNote';
+import ReadNotes from './components/ReadNotes';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import EditNote from './components/EditNote';
 
 function App() {
   
-  // define a state
   const [notes, setNotes] = useState([]);
   const [newNoteContent, setNewNoteContent] = useState('');
   const [newNoteImportant, setNewNoteImportant] = useState('');
   const [showStatus, setShowStatus] = useState('all');
 
-  // runs always
+  const padding = {
+    padding: 15
+  };
+
   useEffect(() => {
     axios
       .get('http://localhost:3001/notes/')
       .then(response => setNotes(response.data));
   });
 
-  // create a reference for the first input text box
   const newNoteContentRef = useRef(null);
 
-  // define the addNote method
   let addNote = (event) => {
     event.preventDefault();
     
-    // create a new object
     let noteObject = {
       id: notes.length + 1,
       content: newNoteContent,
       important: newNoteImportant==='true',
     }
 
-    // add the new object to the notes state
-    // setNotes(notes.concat(noteObject));
-
-    // send the record to the json server
     axios
       .post('http://localhost:3001/notes/', noteObject)
-      .then(response => console.log(response));
-
-    // console.log(noteObject);
-
-    // clear the input text box
+      // .then(response => console.log(response));
+    
     setNewNoteContent('');
     setNewNoteImportant('');
     newNoteContentRef.current.focus();
@@ -63,73 +53,25 @@ function App() {
 
   let handleStatusChange = (event) => {
     setShowStatus(event.target.value);
-    // console.log(event.target.value);
   }
-
-  let filterNotes = (notes, status) => {
-    switch (status) {
-      case 'all':
-        return notes;
-      case 'imp':
-        return notes.filter(note => note.important === true);
-      case 'nonimp':
-        return notes.filter(note => note.important === false);
-    }
-  }
-
-  const notesFiltered = filterNotes(notes, showStatus);
 
   return (
-    <div>
-      <h1>Notes</h1>
+    <Router>
+      
+      <div>
+        <Link style={padding} to="/">Home</Link>
+        <Link style={padding} to="/read">Read Notes</Link>
+        <Link style={padding} to="/create">Create Notes</Link>
+        <Link style={padding} to="/edit">Edit Notes</Link>
+      </div>
 
-      <label>
-        <input type='radio' name='filter' value="all" checked={ showStatus === 'all' } onChange={handleStatusChange} />
-        Show All Notes
-      </label>
-
-      <label>
-        <input type='radio' name='filter' value="imp" checked={ showStatus === 'imp' } onChange={handleStatusChange} />
-        Show Important Notes
-      </label>
-
-      <label>
-        <input type='radio' name='filter' value="nonimp" checked={ showStatus === 'nonimp' } onChange={handleStatusChange} />
-        Show Non-Important Notes
-      </label>
-
-      <ul>
-        {notesFiltered.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-
-      {/* add a simple form for adding notes */}
-      <form onSubmit={addNote}>
-        <input
-          value={newNoteContent}
-          onChange={handleNoteChange}
-          placeholder='type a note...' 
-          ref={newNoteContentRef}
-          /> <br /><br />
-        {/* <input
-          type='text'
-          placeholder='enter true or false'
-          value={newNoteImportant}
-          onChange={(e) => setNewNoteImportant(e.target.value)} /> */}
-        <label form='dropdownNoteImportant'>Is Important: </label>
-        <select
-          id='dropdownNoteImportant'
-          onChange={handleSelectChange}
-          value={newNoteImportant}>
-          <option>--Select--</option>
-          <option>true</option>
-          <option>false</option>
-        </select>
-        <br /><br />
-        <button type='submit'>Add New Note</button>
-      </form>
-    </div>
+      <Routes>
+        <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes} />} />
+        <Route path='/create' element={<CreateNote addNote={addNote} newNoteContent={newNoteContent} handleNoteChange={handleNoteChange} newNoteContentRef={newNoteContentRef} handleSelectChange={handleSelectChange} newNoteImportant={newNoteImportant} />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/edit' element={<EditNote />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/CreateNote.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function CreateNote({addNote, newNoteContent, handleNoteChange, newNoteContentRef, handleSelectChange, newNoteImportant}) {
+  return (
+      <div>
+          <h1>Create Notes</h1>
+          <form onSubmit={addNote}>
+            <input
+            value={newNoteContent}
+            onChange={handleNoteChange}
+            placeholder='type a note...' 
+            ref={newNoteContentRef}
+            /> <br /><br />
+            
+            <label form='dropdownNoteImportant'>Is Important: </label>
+            <select
+            id='dropdownNoteImportant'
+            onChange={handleSelectChange}
+            value={newNoteImportant}>
+            <option>--Select--</option>
+            <option>true</option>
+            <option>false</option>
+            </select>
+            <br /><br />
+            <button type='submit'>Add New Note</button>
+        </form>
+    </div>
+  )
+}
+
+export default CreateNote;
