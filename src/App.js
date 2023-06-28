53c13953b5e5267e0b2de411b6aa67ceee9dbc55
githubscientist <sathish.datascientist@gmail.com>
2023-06-28 10:00:51 +0530

    forwarding redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div></div>
-  )
+const App = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state);
+
+    return (
+        <div>
+            <div>
+                {counter}
+            </div>
+
+            <button onClick={e => dispatch({ type: 'INCREMENT' })}>plus</button>
+            <button onClick={e => dispatch({ type: 'DECREMENT' })}>minus</button>
+            <button onClick={e => dispatch({ type: 'ZERO' })}>zero</button>
+        </div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
 
-// parameters = current state of the application and an action
-// returns: new state of the application
-// const counterReducer = (state, action) => {
-//     if (action.type == 'INCREMENT') {
-//         return state + 1;
-//     } else if (action.type == 'DECREMENT') {
-//         return state - 1;
-//     } else if (action.type == 'ZERO') {
-//         return 0;
-//     }
-//     return state;
-// }
-
-// define the reducer function
-// provide: current state and an action
-// return: new state
 const counterReducer = (state=0, action) => {
     switch (action.type) {
         case 'INCREMENT':
@@ -32,21 +17,10 @@ const counterReducer = (state=0, action) => {
     }
 }
 
-// create a new store
 const store = createStore(counterReducer);
 
-store.subscribe(() => {
-    const storeNow = store.getState();
-    console.log(storeNow);
-})
-
-// get the current state from store
-
-// perform actions
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'ZERO' });
-store.dispatch({ type: 'DECREMENT' });
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
