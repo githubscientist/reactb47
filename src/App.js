a2abcfce36d0251791b0c5f634e35d384eaf14ec
githubscientist <sathish.datascientist@gmail.com>
2023-06-28 21:10:23 +0530

    redux store from index to various components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,38 @@
 import React from 'react';
-import { createStore } from 'redux';
-
-// define the reducer
-const noteReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'NEW_NOTE':
-            // return state.concat(action.payload);
-            return [...state, action.payload];
-        case 'TOGGLE_IMPORTANCE':
-            // find the id to toggle the importance
-            // fetch the action payload id to update the importance
-            const id = action.payload.id;
-
-            // find the object to toggle
-            const noteToChange = state.find(note => note.id === id);
-
-            // toggle the importance field in noteToChange object
-            const changedNote = {
-                ...noteToChange,
-                important: !noteToChange.important
-            }
-
-            return state.map(note => 
-                note.id === id ? changedNote : note
-            )
-
-        default:
-            return state;
-    }
-}
-
-// create a store
-const store = createStore(noteReducer);
-
-// dispatch actions
-store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-        content: 'the app state is in redux store',
-        important: true,
-        id: 1
-    }
-});
-
-store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-        content: 'state changes are made with actions',
-        important: false,
-        id: 2
-    }
-});
-
-store.dispatch({
-    type: 'TOGGLE_IMPORTANCE',
-    payload: {
-        id: 1
-    }
-});
-
-store.dispatch({
-    type: 'TOGGLE_IMPORTANCE',
-    payload: {
-        id: 2
-    }
-});
+import { createNote, toggleImportanceOf } from './reducers/noteReducer';
+import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
+
+    const dispatch = useDispatch();
+    const notes = useSelector(state => state);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const content = event.target.note.value;
+        dispatch(createNote(content));
+        event.target.note.value = '';
+        event.target.note.focus();
+    }
+
+    const toggleImportance = (id) => {
+        dispatch(toggleImportanceOf(id));
+    }
 
     return (
         <div>
             <h1>Notes</h1>
+            <form onSubmit={addNote}>
+                <input name='note' placeholder='type a new note...'/>
+                <button type='submit'>add</button>
+            </form>
             <ul>
                 {
-                    store.getState().map(note =>
-                        <li key={note.id}>
+                    notes.map(note =>
+                        <li
+                            key={note.id}
+                            onClick={() => toggleImportance(note.id)}
+                        >
                             {note.content} <strong>{note.important ? 'important' : ''}</strong>
                         </li>)
                 }
