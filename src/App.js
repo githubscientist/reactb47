1cd4bcfe5aa7f4e9462e4803e7a49266b14c1aef
githubscientist <sathish.datascientist@gmail.com>
2023-06-28 08:44:11 +0530

    redux simple example

--- src/App.js
+++ src/App.js
@@ -1,80 +1,8 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import CreateNote from './components/CreateNote';
-import ReadNotes from './components/ReadNotes';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import EditNote from './components/EditNote';
-import DeleteNote from './components/DeleteNote';
+import React from 'react';
 
 function App() {
-  
-  const [notes, setNotes] = useState([]);
-  const [newNoteContent, setNewNoteContent] = useState('');
-  const [newNoteImportant, setNewNoteImportant] = useState('');
-  const [showStatus, setShowStatus] = useState('all');
-
-  const padding = {
-    padding: 15
-  };
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes/')
-      .then(response => setNotes(response.data));
-  });
-
-  const newNoteContentRef = useRef(null);
-
-  let addNote = (event) => {
-    event.preventDefault();
-    
-    let noteObject = {
-      id: notes.length + 1,
-      content: newNoteContent,
-      important: newNoteImportant==='true',
-    }
-
-    axios
-      .post('http://localhost:3001/notes/', noteObject)
-      // .then(response => console.log(response));
-    
-    setNewNoteContent('');
-    setNewNoteImportant('');
-    newNoteContentRef.current.focus();
-  }
-
-  let handleNoteChange = (event) => {
-    setNewNoteContent(event.target.value);
-  }
-
-  let handleSelectChange = (event) => {
-    setNewNoteImportant(event.target.value);
-  }
-
-  let handleStatusChange = (event) => {
-    setShowStatus(event.target.value);
-  }
-
   return (
-    <Router>
-      
-      <div>
-        <Link style={padding} to="/">Home</Link>
-        <Link style={padding} to="/read">Read Notes</Link>
-        <Link style={padding} to="/create">Create Notes</Link>
-        <Link style={padding} to="/edit">Edit Note</Link>
-        <Link style={padding} to="/delete">Delete Note</Link>
-      </div>
-
-      <Routes>
-        <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes} />} />
-        <Route path='/create' element={<CreateNote addNote={addNote} newNoteContent={newNoteContent} handleNoteChange={handleNoteChange} newNoteContentRef={newNoteContentRef} handleSelectChange={handleSelectChange} newNoteImportant={newNoteImportant} />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/edit' element={<EditNote />} />
-        <Route path='/delete' element={<DeleteNote />} />
-      </Routes>
-    </Router>
+    <div></div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createStore } from 'redux';
 
-// data
-// const notes = [
-//     {
-//         id: 1,
-//         content: 'Working with Lists',
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: 'Usage of keys',
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: 'Conditional Rendering',
-//         important: true
-//     },
-//     {
-//         id: 4,
-//         content: 'Handling Events',
-//         important: true
-//     },
-//     {
-//         id: 5,
-//         content: 'Props Vs State',
-//         important: false
+// parameters = current state of the application and an action
+// returns: new state of the application
+// const counterReducer = (state, action) => {
+//     if (action.type == 'INCREMENT') {
+//         return state + 1;
+//     } else if (action.type == 'DECREMENT') {
+//         return state - 1;
+//     } else if (action.type == 'ZERO') {
+//         return 0;
 //     }
-// ];
+//     return state;
+// }
+
+// define the reducer function
+// provide: current state and an action
+// return: new state
+const counterReducer = (state=0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        case 'ZERO':
+            return 0;
+        default:
+            return state;
+    }
+}
+
+// create a new store
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+    const storeNow = store.getState();
+    console.log(storeNow);
+})
+
+// get the current state from store
+
+// perform actions
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'ZERO' });
+store.dispatch({ type: 'DECREMENT' });
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
